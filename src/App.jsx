57070710a15537ba1eace6ee7591402d73e6e11c
Levit Kanner <lkanner21@gmail.com>
2023-02-17 09:21:16 +0000

    Set up project font family

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 
 function App() {
 
-  return (
-    <div className="App">
-      <p> Hello there </p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <p className={'font-monument text-5xl animate-bounce'}> Hello there </p>
+            <p>I'm a software engineer with 3+ years experience</p>
+        </div>
+    )
 }
 
 export default App
