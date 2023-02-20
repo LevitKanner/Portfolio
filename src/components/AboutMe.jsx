7797fcdcd362d896bd189bbf4dbfcf8aa8ca93d3
Levit Kanner <lkanner21@gmail.com>
2023-02-20 20:04:20 +0000

    Add about me section

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+const AboutMe = () => {
+    return (
+        <div id={'aboutMe'}>
+            <p className={'text-start'}>
+                Levit Osei-Wusu is a fullstack developer with a passion for creating innovative, user-friendly web applications.
+                I have experience in both front-end and back-end development, and specialize in developing applications that
+                are secure, scalable, and performant.
+            </p>
+
+            <p className={'mt-[30px]'}>
+                My experience covers a wide range of technologies, including HTML, CSS,
+                JavaScript, Node.js, React, .Net Core, and other related frameworks. I am constantly learning and
+                exploring new technologies to stay ahead of the curve and give my clients the best possible product.
+            </p>
+
+        </div>
+    )
+};
+
+export default AboutMe;
