9b9b62127fd7b39a006a54f6c24cd0e4e52423ce
Levit Osei-Wusu <lkanner21@gmail.com>
2023-02-21 19:32:46 +0000

    Add personal signature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 const Footer = () => {
     return (
         <div className={'px-[20px] sm:px-[40px]'}>
-            <p className={'w-full text-center p-[20px] border-t border-t-black font-light text-[13px]'}>
-                {new Date().getFullYear()} &copy; all
+            <p className={'flex justify-between items-center w-full text-center py-[20px] border-t border-t-black font-light text-[13px] sm:text-[16px]'}>
+               <span>{new Date().getFullYear()} &copy; all
                 rights
-                reserved.</p>
+                reserved.</span>
+                <span
+                    className={'font-Restonasi text-[20px] sm:text-[35px] font-black lowercase'}> Levit Osei-Wusu</span>
+            </p>
         </div>
     )
 };
