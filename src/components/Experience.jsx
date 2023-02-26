dabf45697458c72bfe4c9287f8f9adb39ff0981a
Levit Osei-Wusu <lkanner21@gmail.com>
2023-02-26 14:15:21 +0000

    Add work experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,16 @@
+const Experience = ({date, company, role, location}) => {
+    return (
+        <div className={'border-t border-t-black py-[45px] flex justify-between items-center px-[20px]'}>
+            <span className={'text-[8px] sm:text-[15px] hidden xl:block'}> [{date}] </span>
+            <div className={'flex flex-col space-y-1.5 xl:space-y-0'}>
+                <span className={'text-[10px] sm:text-[30px] font-monumentLarge'}> {company}</span>
+                <span className={'text-[8px] sm:text-[15px] xl:hidden'}> [{date}] </span>
+            </div>
+
+            <span className={'text-[8px] sm:text-[15px]'}> {role} </span>
+            <span className={'text-[8px] sm:text-[15px]'}> {location}</span>
+        </div>
+    );
+}
+
+export default Experience;
