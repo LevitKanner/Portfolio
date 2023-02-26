e048e512482d92a61d5c279431a14bfdd50c0577
Levit Osei-Wusu <lkanner21@gmail.com>
2023-02-26 14:15:40 +0000

    Add work history section

---
+++ src/pages/WorkHistory.jsx
@@ -0,0 +1,38 @@
+import Experience from "../components/Experience.jsx";
+
+function WorkHistory() {
+    const history = [
+        {
+            company: 'Guaranty Trust Bank Ghana',
+            date: 'jul 2021 - mar 2023',
+            role: 'applications developer',
+            location: 'on-site'
+        },
+        {
+            company: 'Appgharage Limited',
+            date: 'mar 2021 - sept 2021',
+            role: 'Web developer',
+            location: 'remote'
+        },
+        {
+            company: 'Nikasemo Technologies',
+            date: 'Aug 2019 - feb 2021',
+            role: 'IOS mobile developer',
+            location: 'hybrid'
+        },{
+            company: 'Polymorph labs',
+            date: 'jun 2019 - Aug 2019',
+            role: 'Backend Intern',
+            location: 'on-site'
+        },
+    ]
+    return (
+        <div className={'px-[20px] sm:px-[40px]'}>
+            {history.map((h, i) => (
+                <Experience key={i} company={h.company} role={h.role} location={h.location} date={h.date} />
+            ))}
+        </div>
+    );
+}
+
+export default WorkHistory;
