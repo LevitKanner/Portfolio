53bf7aaf6eb9d2abba6de51ef39739f27c52bac2
Levit Osei-Wusu <lkanner21@gmail.com>
2023-03-19 14:40:31 +0000

    Add tools section

---
+++ src/components/Tools.jsx
@@ -0,0 +1,9 @@
+const Tools = () => {
+    return (
+        <div className={'px-[20px] sm:px-[40px] mt-16'}>
+            <p className={'secondary-title'}> Tools I work with</p>
+        </div>
+    );
+};
+
+export default Tools;
